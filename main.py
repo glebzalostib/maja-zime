8f127c2cc970611fb06228de39eb1700e573709c
Glebalostib <77092459+glebzalostib@users.noreply.github.com>
2022-01-31 12:44:25 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+import turtle
+Rafael = turtle.Turtle()
+Rafael.color("red")
+Rafael.forward(60)
+Rafael.left(90)
+Rafael.forward(50)
+Rafael.left(90)
+Rafael.forward(20)
+Rafael.right(135)
+Rafael.forward(70)
+Rafael.right(90)
+Rafael.forward(70)
+Rafael.right(135)
+Rafael.forward(20)
+Rafael.left(90)
+Rafael.forward(50)
+Rafael.left(90)
+Rafael.forward(60)
+Rafael.right(180)
+Rafael.forward(150)
+
+
+
